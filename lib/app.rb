eab684ed7e3c80cce01e6a7d15d475962962e6c1
Gabriel Malaquias <gabriel07malakias@gmail.com>
2019-01-24 21:22:52 +0100

    Liveness, Readiness and Secrets

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
+require 'json'
 
 set :bind, '0.0.0.0'
 
 get '/' do
-  'app says: AiO!'
+  "app says: AiO! Secrets username #{ENV['USERNAME']} password: #{ENV['PASSWORD']}"
+end
+
+get '/health_check' do
+  content_type :json
+
+  { status: "ok" }.to_json
 end
