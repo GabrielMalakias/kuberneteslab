cb34c44c798a4333a8d343fc2685522dd80eacaa
Gabriel Malaquias <gabriel07malakias@gmail.com>
2019-02-14 20:47:20 +0100

    Some changes

--- ruby_app/lib/app.rb
+++ ruby_app/lib/app.rb
@@ -1,11 +1,21 @@
 require 'sinatra'
 require 'json'
+require 'pg'
 require_relative 'app/client/elixir_app'
 
 set :bind, '0.0.0.0'
 
+result = PG::Connection.ping(ENV['POSTGRES_HOST'],
+                             5432,
+                             {},
+                             false,
+                             ENV['POSTGRES_DATABASE'],
+                             ENV['POSTGRES_USER'],
+                             ENV['POSTGRES_PASSWORD'])
+
 get '/' do
   <<-BODY
+  \nDATABASE result: #{result}
   \nRunning Ruby App: Env Vars #{ENV['USERNAME']} password: #{ENV['PASSWORD']}
   \nElixir: #{ENV['ELIXIRAPPSERVICE_SERVICE_HOST']}
   \nRequest from Elixir App: #{App::Client::ElixirApp.new.get}
